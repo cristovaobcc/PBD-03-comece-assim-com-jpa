13401c02871ac1c0cf171c14abc588ec710bfda2
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-26 09:03:54 -0300

    Criação da classe Clientes com anotações JPA.

---
+++ src/main/java/com/algaworks/cadastrocliente/model/Cliente.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package com.algaworks.cadastrocliente.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Representa um Cliente em 
+ *
+ */
+@Entity
+@Table(name = "cliente") 
+public class Cliente {
+	
+	@Id
+//	@Column(name = "cli_id")
+	private Integer id;
+	
+//	@Column(name = "cli_nome")
+	private String nome;
+
+	
+	
+	
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Cliente)) {
+			return false;
+		}
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+}
