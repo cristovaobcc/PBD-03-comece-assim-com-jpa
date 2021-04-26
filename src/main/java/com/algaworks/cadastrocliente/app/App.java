4f54f93b69893c118be780407498449a47541b61
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-26 10:59:39 -0300

    Atualizando registros de forma gerenciada e não gerenciada.
    Também é feito o uso do merge() como insert.
    
    Lembre que, nos testes dessa app, na linha 51 vc tem que alterar o find
    conforme o id do cliente que quer remover, senão vc terá NPE no console.

--- src/main/java/com/algaworks/cadastrocliente/app/App.java
+++ src/main/java/com/algaworks/cadastrocliente/app/App.java
@@ -48,11 +48,38 @@ public class App {
 		
 		// Removendo um registro
 		// A gente só consegue remover objetos que o entityManager sabe que existem(já está na mem[oria dele, sob seu gerenciamento)
-		cliente = entityManager.find(Cliente.class, 2); // automaticamente o entityManager coloca o objeto buscado como gerenciado
+		cliente = entityManager.find(Cliente.class, 6); // automaticamente o entityManager coloca o objeto buscado como gerenciado
 		
 		entityManager.getTransaction().begin();
 		entityManager.remove(cliente);
 		entityManager.getTransaction().commit();
+		
+		// Atualizando um registro de forma gerenciada
+		cliente = entityManager.find(Cliente.class, 1);
+		
+		entityManager.getTransaction().begin();
+		cliente.setNome(cliente.getNome() + " Alterado");
+		entityManager.getTransaction().commit();
+		
+		// Atualizando um registro de forma não gerenciada
+		// Suponha que vc montou o seu novo objeto com base nas informações vindas do front-end:
+		cliente = new Cliente(); // note que este objeto não está gerenciado pelo entityManager(ou seja, não está em sua memória)
+		cliente.setId(1);
+		cliente.setNome("Armazem Estrela");
+		
+		entityManager.getTransaction().begin();
+		entityManager.merge(cliente); // Não é obrigatório estar entre begin() commit(), mas é recomendado.
+		entityManager.getTransaction().commit();
+		
+		
+		// O merge() pode ser usado para fazer inserção, caso vc não passe o id. Vide;
+		cliente = new Cliente();
+		cliente.setNome("Construtora Ferreira");
+		
+		entityManager.getTransaction().begin();
+		entityManager.merge(cliente);
+		entityManager.getTransaction().commit();
+			
 		
 		// Fechando entityManager e entityManagerFactory.
 		entityManager.close();
