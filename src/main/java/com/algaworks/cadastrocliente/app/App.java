2878d504e1a9455b17ebec4383d914164108bd26
cristovaobcc <cristovao.bcc@gmail.com>
2021-04-26 09:19:20 -0300

    Persistindo um cliente com id inserido manualmente.
    Rode a app e atente ao SQL gerado.

--- src/main/java/com/algaworks/cadastrocliente/app/App.java
+++ src/main/java/com/algaworks/cadastrocliente/app/App.java
@@ -31,8 +31,20 @@ public class App {
 		// junto com o início e o fim da  requisição, respectivamente.
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
+		// Buscando um cliente no DB.
 		Cliente cliente = entityManager.find(Cliente.class, 1);
 		System.out.println(cliente.getNome());
+		
+		// Inserindo registro
+		cliente = new Cliente();
+		cliente.setId(2); // inserção manual de id. Mas lembre que a tabela é autoincrementável nesse campo.
+		cliente.setNome("Autopeças Estrada");
+		
+		entityManager.getTransaction().begin();
+		
+		entityManager.persist(cliente);
+		
+		entityManager.getTransaction().commit();
 		
 		// Fechando entityManager e entityManagerFactory.
 		entityManager.close();
